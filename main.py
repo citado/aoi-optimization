787a6ec567ee7cb6f5553e1ae530f569c5de9e99
Parham Alvani <parham.alvani@gmail.com>
2022-07-27 09:58:21 +0430

    feat: update model

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ try:
     m = gp.Model("aoi")
 
     subchannels = 5
-    slots = 10
-    things = 20
+    slots = 3
+    things = 10
 
     x = m.addVars(things, slots, vtype=GRB.INTEGER, name="x")
     p = m.addVars(things, subchannels, slots, vtype=GRB.BINARY, name="p")
@@ -28,12 +28,15 @@ try:
     )
 
     m.addConstrs(
-        p.sum(i, "*", t) <= 1
-        for (i, t) in itertools.product(range(things), range(slots))
+        (
+            p.sum(i, "*", t) <= 1
+            for (i, t) in itertools.product(range(things), range(slots))
+        ),
+        name="thing_limit",
     )
 
     m.addConstrs(
-        -GRB.INFINITY * sum(p[i, "*", t]) + x[i, t] + 1 <= x[i, t + 1]
+        -GRB.INFINITY * p.sum(i, "*", t) + x[i, t] + 1 <= x[i, t + 1]
         for (i, t) in itertools.product(range(things), range(slots - 1))
     )
 
@@ -41,10 +44,25 @@ try:
 
     m.optimize()
 
-    for v in m.getVars():
-        print(f"{v.VarName}, {v.X}")
+    for t in range(slots):
+        print(f"slot {t}:")
+
+        for i in range(things):
+            x = m.getVarByName(f"x[{i},{t}]")
+            print(f"\tage of information for thing {i}: {x.X}")
+
+    for t in range(slots):
+        print(f"slot {t}:")
+
+        for i in range(things):
+            for c in range(subchannels):
+                p = m.getVarByName(f"p[{i},{c},{t}]")
+                if p.X == 1:
+                    print(f"\tthing {i} is using subchannel {c}")
 
     print(f"Obj: {m.ObjVal}")
+
+    m.write("model.lp")
 
 except gp.GurobiError as e:
     print(f"Error code {e.errno}: {e}")
